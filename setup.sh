ecb4a07dfb7d5705ba85a7bf01ab42ba59ffb6aa
bupt <bupt>
2025-07-29 15:03:14 +0800

    fully

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # 开机自启动脚本,不要随便修改!
-bash /home/bupt/fly_ws/start360.sh
 
 # mavros
 tmux new-session -d -s mavros_session
 tmux send-keys -t mavros_session:0 'roslaunch mavros px4.launch' C-m
 
+sleep 1
+bash /home/bupt/fly_ws/start360.sh
+
+sleep 1
+
+tmux new-session -d -s offboard
+tmux send-keys -t offboard:0 'source /home/bupt/fly_ws/devel/setup.bash && \
+ roslaunch offboard_run run_offboard.launch' C-m
